84360bf53455fe8e9c74bee46f4599799ae26a7b
AsifNawazLashari <asiflashari143@gmail.com>
2024-03-29 20:21:17 +0500

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,19 +1,14 @@
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-import { initializeApp } from "firebase/app";
-
-// Add the Firebase products that you want to use
-import { getAnalytics } from "firebase/analytics";
-
-// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBpZfPitBi12rkyzfyV1-scSvCc32jFAWE",
   authDomain: "science-ki-duniya.firebaseapp.com",
+  databaseURL: "https://science-ki-duniya-default-rtdb.firebaseio.com",
   projectId: "science-ki-duniya",
   storageBucket: "science-ki-duniya.appspot.com",
   messagingSenderId: "271115609150",
   appId: "1:271115609150:web:a0dfc810aee52a978cdd6f",
   measurementId: "G-KBHX5S9PX5"
-};
+};"
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
